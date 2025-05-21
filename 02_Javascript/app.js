6032cfb1f25418dd692b1908d4820c02720bc14d
Mareola <bulun1001@gmail.com>
2025-05-22 00:03:44 +0100

    feat: add javascript objects

---
+++ 02_Javascript/app.js
@@ -0,0 +1,12 @@
+const user = {
+    name: "John Doe",
+    age: 30,
+    email: "john.doe@example.com"
+}
+
+user["age"] = 31;
+user["account_no"] = 4832076010
+user["bankName"] = "FCMB"
+
+console.log(user);
+console.log(user.bankName);
