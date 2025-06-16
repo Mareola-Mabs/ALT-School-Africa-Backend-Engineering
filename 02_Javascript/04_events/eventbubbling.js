4898931a31bbb01c16025427383f5abe1ba4338c
Mareola <bulun1001@gmail.com>
2025-06-16 13:56:27 +0100

    feat: add more notes on events

--- 02_Javascript/04_events/eventbubbling.js
+++ 02_Javascript/04_events/eventbubbling.js
@@ -7,3 +7,19 @@ document.querySelector("button").addEventListener("click", (e)=>{
 document.querySelector("main").addEventListener("click", ()=>{
     console.log("You Clicked the Parent")
 })
+
+
+document.querySelectorAll(".container button")[0].addEventListener("click", (e)=>{
+    e.stopPropagation()
+    console.log("ok")
+
+})
+
+document.querySelector(".container").addEventListener("click", (e)=>{
+    e.cancelBubble = true
+    console.log(e.target.innerHTML, e.tiltX, e.type, e.cancelBubble)
+
+    
+})
+
+console.log(screen.availWidth, screen.width, window.innerWidth)
